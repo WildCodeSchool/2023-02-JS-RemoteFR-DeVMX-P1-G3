69c83079bbb18a49d0b67d74a9454ee01df413d5
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-03-19 18:40:39 +0100

    Function improvement for action modal profil and uptdate class style

--- script.js
+++ script.js
@@ -35,117 +35,25 @@ slider.addEventListener('mousemove', e => {
 
 /*****************************modal1********************************* */
 
-let modalProfil1 = document.getElementById("modal1");
-let spanModalProfil1 = document.getElementById("btn_close1");
-let btnModalProfil1 = document.getElementById("btn_open1");
+let modalProfil = document.querySelectorAll(".modal");
+let closeModalProfil = document.querySelectorAll(".close_modal_profil");
+let btnModalProfil = document.querySelectorAll(".btn_open_profil");
 
-btnModalProfil1.onclick = function () {
-    modalProfil1.style.display = "block";
-};
+btnModalProfil.forEach(function (btn) {
+    btn.onclick = (function () {
+        let modal = btn.getAttribute("data-target");
+        document.getElementById(modal).style.display = "block";
+    });
+});
+closeModalProfil.forEach(function(btn) {
+    btn.onclick = function() {
+        let modal = (btn.closest(".modal").style.display = "none");
+        
+    };
+});
 
-spanModalProfil1.onclick = function() {
-    modalProfil1.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target === modalProfil1) {
-        modalProfil1.style.display = "none";
+window.onclick = function (e){
+    if (e.target.className === "modal"){
+        e.target.style.display = "none";
     }
-}
-
-/**********************************modal 2************************** */
-
-let modalProfil2 = document.getElementById("modal2");
-let spanModalProfil2 = document.getElementById("btn_close2");
-let btnModalProfil2 = document.getElementById("btn_open2");
-
-btnModalProfil2.onclick = function () {
-    modalProfil2.style.display = "block";
 };
-
-spanModalProfil2.onclick = function() {
-    modalProfil2.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target === modalProfil2) {
-        modalProfil2.style.display = "none";
-    }
-}
-
-/**********************************modal 3************************** */
-
-let modalProfil3 = document.getElementById("modal3");
-let spanModalProfil3 = document.getElementById("btn_close3");
-let btnModalProfil3 = document.getElementById("btn_open3");
-
-btnModalProfil3.onclick = function () {
-    modalProfil3.style.display = "block";
-};
-
-spanModalProfil3.onclick = function() {
-    modalProfil3.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target === modalProfil3) {
-        modalProfil3.style.display = "none";
-    }
-}
-/**********************************modal 4************************** */
-
-let modalProfil4 = document.getElementById("modal4");
-let spanModalProfil4 = document.getElementById("btn_close4");
-let btnModalProfil4 = document.getElementById("btn_open4");
-
-btnModalProfil4.onclick = function () {
-    modalProfil4.style.display = "block";
-};
-
-spanModalProfil4.onclick = function() {
-    modalProfil4.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target === modalProfil4) {
-        modalProfil4.style.display = "none";
-    }
-}
-/**********************************modal 5************************** */
-
-let modalProfil5 = document.getElementById("modal5");
-let spanModalProfil5 = document.getElementById("btn_close5");
-let btnModalProfil5 = document.getElementById("btn_open5");
-
-btnModalProfil5.onclick = function () {
-    modalProfil5.style.display = "block";
-};
-
-spanModalProfil5.onclick = function() {
-    modalProfil5.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target === modalProfil5) {
-        modalProfil5.style.display = "none";
-    }
-}
-/**********************************modal 6************************** */
-
-let modalProfil6 = document.getElementById("modal6");
-let spanModalProfil6 = document.getElementById("btn_close6");
-let btnModalProfil6 = document.getElementById("btn_open6");
-
-btnModalProfil6.onclick = function () {
-    modalProfil6.style.display = "block";
-};
-
-spanModalProfil6.onclick = function() {
-    modalProfil6.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target === modalProfil6) {
-        modalProfil6.style.display = "none";
-    }
-}
